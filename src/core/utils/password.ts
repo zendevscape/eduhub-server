f7e34fec27493a8ebabc8b611c54488b283ad56f
Ahmad Rulim <mfalkia342@gmail.com>
2021-10-18 23:16:15 +0700

    Set argon2 hash type to argon2id
    Signed-off-by: Ahmad Rulim <mfalkia342@gmail.com>

--- src/core/utils/password.ts
+++ src/core/utils/password.ts
@@ -1,12 +1,12 @@
 import argon2 from 'argon2';
 
 export const hashPassword = async (password: string): Promise<string> => {
-  return await argon2.hash(password);
+  return await argon2.hash(password, { type: argon2.argon2id });
 };
 
 export const verifyPassword = async (
   inputPassword: string,
   referencePassword: string,
 ): Promise<boolean> => {
-  return await argon2.verify(referencePassword, inputPassword);
+  return await argon2.verify(referencePassword, inputPassword, { type: argon2.argon2id });
 };
