d634a6ac2c49c5b8a330aaf4ac34ec51fac46533
Ahmad Rulim <mfalkia342@gmail.com>
2021-11-20 00:33:14 +0700

    Add explicit accessibility modifier
    Signed-off-by: Ahmad Rulim <mfalkia342@gmail.com>

--- src/features/payment-gateways/services/payment-gateways.service.ts
+++ src/features/payment-gateways/services/payment-gateways.service.ts
@@ -1,7 +1,10 @@
 import { CreatePaymentReq, PaymentRes, ReceiveCallbacksReq } from '../dtos';
 
 export abstract class PaymentGatewaysService {
-  abstract createPayment(payment: CreatePaymentReq): Promise<PaymentRes>;
+  public abstract createPayment(payment: CreatePaymentReq): Promise<PaymentRes>;
 
-  abstract receiveCallbacks(headers: ReceiveCallbacksReq, body: ReceiveCallbacksReq): Promise<void>;
+  public abstract receiveCallbacks(
+    headers: ReceiveCallbacksReq,
+    body: ReceiveCallbacksReq,
+  ): Promise<void>;
 }
